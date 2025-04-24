0e3e58ebaa72c085bfa970a670dc1e22dfb1419d
Hilaladiii <abdulhilal242@gmail.com>
2025-04-24 17:20:14 +0700

    feat : add supabase module

---
+++ src/modules/supabase/supabase.service.ts
@@ -0,0 +1,39 @@
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { createClient, SupabaseClient } from '@supabase/supabase-js';
+
+@Injectable()
+export class SupabaseService {
+  private supabaseClient: SupabaseClient;
+
+  constructor(private configService: ConfigService) {
+    this.supabaseClient = createClient(
+      this.configService.get('SUPABASE_URL'),
+      this.configService.get('SUPABASE_KEY'),
+    );
+  }
+
+  async upload(
+    file: Express.Multer.File,
+    bucket: string,
+  ): Promise<{ publicUrl: string }> {
+    try {
+      const { error } = await this.supabaseClient.storage
+        .from(bucket)
+        .upload(file.originalname, file.buffer, {
+          contentType: file.mimetype,
+          upsert: true,
+        });
+
+      if (error) throw new InternalServerErrorException();
+
+      const { data } = this.supabaseClient.storage
+        .from(bucket)
+        .getPublicUrl(file.originalname);
+
+      return data;
+    } catch (error) {
+      throw new InternalServerErrorException(error.message);
+    }
+  }
+}
