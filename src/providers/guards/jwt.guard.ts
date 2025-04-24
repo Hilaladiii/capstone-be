aab8ef26539c37a3faa40b75a0b988b74c5ab8ef
Hilaladiii <abdulhilal242@gmail.com>
2025-04-24 17:20:38 +0700

    feat : add jwt guard (authorization)

---
+++ src/providers/guards/jwt.guard.ts
@@ -0,0 +1,31 @@
+import { ExecutionContext, Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { AuthGuard } from '@nestjs/passport';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class JwtGuard extends AuthGuard('jwt') {
+  constructor(private jwtService: JwtService) {
+    super();
+  }
+
+  canActivate(
+    context: ExecutionContext,
+  ): boolean | Promise<boolean> | Observable<boolean> {
+    try {
+      const request = context.switchToHttp().getRequest();
+      const bearerToken = request.headers.authorization?.replace('Bearer ', '');
+
+      if (!bearerToken) return false;
+
+      const token = this.jwtService.verify(bearerToken);
+      if (!token) return false;
+
+      request.user = token;
+
+      return true;
+    } catch (error) {
+      return false;
+    }
+  }
+}
