5f5110e45fee86a179bc06a50ee110a913e16e78
Hilaladiii <abdulhilal242@gmail.com>
2025-04-25 10:51:48 +0700

    feat : Only admin can create roles

--- src/modules/user/role/role.controller.ts
+++ src/modules/user/role/role.controller.ts
@@ -1,7 +1,11 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
 import { RoleService } from './role.service';
 import { CreateRoleDto } from './dto/create-role.dto';
 import { Message } from 'src/commons/decorators/message.decorator';
+import { Roles } from 'src/commons/decorators/role.decorator';
+import { Role } from 'src/commons/types/role.type';
+import { JwtGuard } from 'src/providers/guards/jwt.guard';
+import { RoleGuard } from 'src/providers/guards/role.guard';
 
 @Controller('user/role')
 export class RoleController {
@@ -9,6 +13,8 @@ export class RoleController {
 
   @Post('create')
   @Message('Success create new role')
+  @Roles(Role.ADMIN)
+  @UseGuards(JwtGuard, RoleGuard)
   async create(@Body() createRoleDto: CreateRoleDto) {
     return await this.roleService.create(createRoleDto);
   }
