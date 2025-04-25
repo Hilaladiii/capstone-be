04ee71112fc1c50d56fbf1d276d3884ea27aa8d5
Hilaladiii <abdulhilal242@gmail.com>
2025-04-25 14:40:24 +0700

    feat : add getRoleByName

--- src/modules/user/student/student.module.ts
+++ src/modules/user/student/student.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { StudentService } from './student.service';
 import { StudentController } from './student.controller';
+import { RoleModule } from '../role/role.module';
 
 @Module({
+  imports: [RoleModule],
   providers: [StudentService],
   controllers: [StudentController],
 })
