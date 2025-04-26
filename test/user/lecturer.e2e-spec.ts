2c9a7e9e440be70d1760cefeddc2305366adb1ef
Hilaladiii <abdulhilal242@gmail.com>
2025-04-26 18:48:03 +0700

    test : add lecturer register testing

---
+++ test/user/lecturer.e2e-spec.ts
@@ -0,0 +1,160 @@
+import { INestApplication } from '@nestjs/common';
+import { createTestApp } from 'test/setup.e2e';
+import * as request from 'supertest';
+import { PrismaService } from 'src/modules/prisma/prisma.service';
+import { LECTURER_TEST_PAYLOAD, loginHelper } from 'test/helper';
+import { LecturerType, StudentType } from 'src/commons/types/user.type';
+
+describe('Lecturer e2e', () => {
+  let app: INestApplication;
+  let prismaService: PrismaService;
+  let headLecturerToken: string;
+  let lecturer: LecturerType;
+  let student: StudentType;
+  let studentToken: string;
+
+  beforeAll(async () => {
+    app = await createTestApp();
+    prismaService = app.get(PrismaService);
+
+    lecturer = await prismaService.user.findFirst({
+      where: {
+        userRoles: {
+          some: {
+            role: {
+              role_name: 'LECTURER',
+            },
+          },
+          none: {
+            role: {
+              role_name: 'HEAD_LECTURER',
+            },
+          },
+        },
+      },
+      include: {
+        lecturer: true,
+      },
+    });
+
+    const resHeadLecturer = await loginHelper(app, {
+      email: 'dosen@gmail.com',
+      password: 'dosen123',
+    });
+
+    headLecturerToken = resHeadLecturer.body.data.token;
+
+    const resStudent = await loginHelper(app, {
+      email: 'student@gmail.com',
+      password: 'student123',
+    });
+
+    studentToken = resStudent.body.data.token;
+
+    student = await prismaService.user.findFirst({
+      include: {
+        student: true,
+      },
+    });
+  });
+
+  afterAll(async () => {
+    await prismaService.student.update({
+      where: {
+        user_id: student.user_id,
+      },
+      data: {
+        lecturer: {
+          disconnect: {
+            nip: lecturer.lecturer.nip,
+          },
+        },
+      },
+    });
+    await prismaService.user.delete({
+      where: {
+        email: LECTURER_TEST_PAYLOAD.email,
+      },
+    });
+    await app.close();
+  });
+
+  describe('/user/lecturer/register (POST)', () => {
+    it('should register lecturer successfully ', async () => {
+      const res = await request(app.getHttpServer())
+        .post('/user/lecturer/register')
+        .send(LECTURER_TEST_PAYLOAD);
+
+      expect(res.status).toBe(201);
+      expect(res.body).toHaveProperty(
+        'message',
+        'Success register your account',
+      );
+      expect(res.body).toHaveProperty('data');
+    });
+
+    it('should return 400 if user is already registered ', async () => {
+      const res = await request(app.getHttpServer())
+        .post('/user/lecturer/register')
+        .send(LECTURER_TEST_PAYLOAD);
+
+      expect(res.status).toBe(400);
+      expect(res.body).toHaveProperty('message', 'User already exists');
+    });
+
+    it('should return 400 if there is a mandatory field that is empty (email)', async () => {
+      const res = await request(app.getHttpServer())
+        .post('/user/lecturer/register')
+        .send({ ...LECTURER_TEST_PAYLOAD, email: undefined });
+
+      expect(res.status).toBe(400);
+      expect(res.body.message).toContainEqual('email must be an email');
+      expect(res.body.message).toContainEqual('email should not be empty');
+    });
+  });
+
+  describe('/user/lecturer/student/:nim/supervisor (POST)', () => {
+    it('should connect student with supervisor successfully', async () => {
+      const res = await request(app.getHttpServer())
+        .post(`/user/lecturer/student/${student.student.nim}/supervisor`)
+        .set('Authorization', `Bearer ${headLecturerToken}`)
+        .send({
+          nip: lecturer.lecturer.nip,
+        });
+
+      expect(res.status).toBe(201);
+      expect(res.body).toHaveProperty(
+        'message',
+        'Success connect supervisor with student',
+      );
+      expect(res.body).toHaveProperty('data');
+    });
+
+    it('should return 403 if not head lecturer', async () => {
+      const res = await request(app.getHttpServer())
+        .post(`/user/lecturer/student/${student.student.nim}/supervisor`)
+        .set('Authorization', `Bearer ${studentToken}`)
+        .send({
+          nip: lecturer.lecturer.nip,
+        });
+
+      expect(res.status).toBe(403);
+      expect(res.body).toHaveProperty('message', 'Forbidden resource');
+    });
+
+    it('should return 404 if student or lecturer not registered', async () => {
+      const res = await request(app.getHttpServer())
+        .post(`/user/lecturer/student/${student.student.nim}/supervisor`)
+        .set('Authorization', `Bearer ${headLecturerToken}`)
+        .send({
+          nip: '1234567890',
+        });
+
+      expect(res.status).toBe(404);
+      expect(res.body).toHaveProperty(
+        'message',
+        'Student or lecturer not found',
+      );
+    });
+  });
+});
