6bc3e3ca68ef6c2b89d2477684e6271795a2c47a
Hilaladiii <abdulhilal242@gmail.com>
2025-05-02 21:42:30 +0700

    feat : add change password feature

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,7 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Put } from '@nestjs/common';
 import { UserService } from './user.service';
+import { ChangePasswordDto } from './dto/change-password.dto';
+import { Auth } from 'src/commons/decorators/auth.decorator';
+import { GetCurrentUser } from 'src/commons/decorators/get-current-user.decorator';
+import { Message } from 'src/commons/decorators/message.decorator';
 
 @Controller('user')
 export class UserController {
   constructor(private userService: UserService) {}
+
+  @Put('change-password')
+  @Message('Success change your password')
+  @Auth()
+  async changePassword(
+    @GetCurrentUser('sub') user_id: string,
+    @Body() changePasswordDto: ChangePasswordDto,
+  ) {
+    console.log(user_id);
+    await this.userService.changePassword({ ...changePasswordDto, user_id });
+  }
 }
--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,7 +1,40 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
+import { ChangePasswordDto } from './dto/change-password.dto';
+import * as bcryptjs from 'bcryptjs';
 
 @Injectable()
 export class UserService {
   constructor(private prismaService: PrismaService) {}
+
+  async changePassword({
+    password,
+    confirmPassword,
+    user_id,
+  }: ChangePasswordDto & {
+    user_id: string;
+  }) {
+    const user = await this.prismaService.user.findUnique({
+      where: { user_id },
+    });
+    if (!user) throw new NotFoundException('User not found');
+
+    if (password !== confirmPassword)
+      throw new BadRequestException(
+        'Password must match with confirm password',
+      );
+
+    const hashedPassword = await bcryptjs.hash(password, 10);
+
+    await this.prismaService.user.update({
+      where: { user_id },
+      data: {
+        password: hashedPassword,
+      },
+    });
+  }
 }
